a6c18cfc22171fb6062bb9520f4367a6abbbc1db
onyenike <onyenikefestus2019@gmail.com>
2025-08-09 19:48:23 +0100

    Add files via upload

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,20 @@
+import Header from './components/Header'
+import './App.css'
+import UserProfile from './components/UserProfile'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
+
+function App() {
+
+
+  return (
+    <>
+    <Header />
+    <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    <MainContent />
+    <Footer />
+    </>
+  )
+}
+
+export default App
