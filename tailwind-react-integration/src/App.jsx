124b5076181f3d0bddc15fad0e9ddccc8c244be0
onyenike <onyenikefestus2019@gmail.com>
2025-08-09 16:31:47 +0100

    Add files via upload

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,19 @@
+
+import About from './About'
+import './App.css'
+import UserProfile from './components/UserProfile'
+
+function App() {
+  return (
+    <>
+      <About />
+       <UserProfile />
+
+
+       
+     
+    </>
+  )
+}
+
+export default App
