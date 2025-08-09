9bd9a4458a21e545afba3408d323d69ed4995dab
onyenike <onyenikefestus2019@gmail.com>
2025-08-09 12:01:48 -0700

    Update App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import UserProfile from './components/UserProfile'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import Counter from './components/Counter'
 
 function App() {
 
