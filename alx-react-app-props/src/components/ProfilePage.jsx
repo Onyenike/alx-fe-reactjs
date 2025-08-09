2589ec95a30b4ddc7b2261d685b81194fde75ac2
onyenike <onyenikefestus2019@gmail.com>
2025-08-09 12:39:44 -0700

    Create ProfilePage.jsx

---
+++ alx-react-app-props/src/components/ProfilePage.jsx
@@ -0,0 +1,7 @@
+import UserInfo from './UserInfo'; // This component will show nested user info
+
+function ProfilePage() {
+  return <UserInfo />; // No need to pass props anymore
+}
+
+export default ProfilePage;;
