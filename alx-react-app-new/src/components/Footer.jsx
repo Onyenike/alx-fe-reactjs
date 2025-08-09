349397a345652e72dfb9c7650387e9bba866d661
onyenike <onyenikefestus2019@gmail.com>
2025-08-09 19:46:33 +0100

    Add files via upload

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return (  
+    <footer style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
+        <p>© 2023 City Lovers</p>
+   </footer>);
+}
+ 
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+const MainContent = () => {
+    return (  
+        <main style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
+         <p>I love to visit New York, Paris, and Tokyo.</p>
+         </main>
+    );
+}
+ 
+export default MainContent;
